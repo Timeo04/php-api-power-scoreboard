ebd298d799fe0e70d76b412089f9425e08e7dc1e
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-19 23:06:29 +0100

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>POWERS - Scoreboard</title>
+ </head>
+ <body>
+   <h1>Scoreboard</h1>
+   <a href="https://openprocessing.org/sketch/1447262">zum Game</a>
+ <?php echo '<p>Hallo Welt</p>'; ?>
+ </body>
+</html>
