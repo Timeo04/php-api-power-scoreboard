4671257fe125bd9a613aef4864003b1720b0f667
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 00:27:17 +0100

    Update add.php

--- web/add.php
+++ web/add.php
@@ -1,1 +1,21 @@
+<?php
+//header('Content-Type: application/json; charset=utf-8');
+$db = parse_url(getenv("DATABASE_URL"));
 
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+// Bekomme den JSON Body der Anfrage
+$data = json_decode(file_get_contents('php://input'), true);
+ 
+// Extrahiere den Text
+$text = trim($data['text'] ?? '');
+
+echo $data;
+echo $text;
