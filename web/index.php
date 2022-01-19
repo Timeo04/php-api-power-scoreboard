e5072f27dd064658db1513d42507df4070299e91
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-19 23:12:40 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,6 +5,24 @@
  <body>
    <h1>Scoreboard</h1>
    <a href="https://openprocessing.org/sketch/1447262">zum Game</a>
- <?php echo '<p>Hallo Welt</p>'; ?>
+ <?php 
+  echo '<p>Hallo Welt</p>'; 
+  
+  $db = parse_url(getenv("DATABASE_URL"));
+  $pdo = new PDO("pgsql:" . sprintf(
+      "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+      $db["host"],
+      $db["port"],
+      $db["user"],
+      $db["pass"],
+      ltrim($db["path"], "/")
+  ));
+  
+  $sql = "SELECT score, name, date FROM scoreboard ORDER BY score DESC";
+  foreach ($pdo->query($sql) as $row) {
+   echo $row['score']." ".$row['name']." ".$row['date']."  "."<br />";
+   //echo "E-Mail: ".$row['email']."<br /><br />";
+  }
+ ?>
  </body>
 </html>
