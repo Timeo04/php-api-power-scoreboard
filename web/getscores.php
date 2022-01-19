908680fcc443663ca7cc202556dd53a42492adda
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-19 23:58:15 +0100

    Update getscores.php

--- web/getscores.php
+++ web/getscores.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+if(isset($_GET['num']){
+    $num = (int)$_GET['num'];
+}
 $db = parse_url(getenv("DATABASE_URL"));
 
 $pdo = new PDO("pgsql:" . sprintf(
@@ -12,7 +15,11 @@ $pdo = new PDO("pgsql:" . sprintf(
 ));
 
 //$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-$statement = $pdo->prepare("SELECT * FROM scoreboard ORDER BY score;");
+if(isset($num) and is_numberic($num)  and $num > 0){
+    $statement = $pdo->prepare("SELECT TOP $num * FROM scoreboard ORDER BY score;");
+}else{
+    $statement = $pdo->prepare("SELECT * FROM scoreboard ORDER BY score;");
+}
 $statement->execute();
 $data = $statement->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($data);
