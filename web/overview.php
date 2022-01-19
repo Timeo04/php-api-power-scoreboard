aeb01289d041b9cdf814cc20310a0f3aacde9848
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-19 22:06:18 +0100

    Update overview.php

--- web/overview.php
+++ web/overview.php
@@ -1,7 +1,17 @@
 <?php
-header('Content-Type: application/json');
-$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-$statement = $pdo->prepare("SELECT * FROM todo ORDER BY id");
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+//$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+$statement = $pdo->prepare("SELECT * FROM scoreboard ORDER BY score;");
 $statement->execute();
 $data = $statement->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($data);
