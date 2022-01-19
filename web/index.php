1615cb88d3e1c620a1452f5772d5cb743dc32715
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 00:59:39 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -33,7 +33,7 @@
   ));
   
   
-  echo '<table border="1" width="200" style="margin-left:auto;margin-right:auto">';
+  echo '<table border="1" width="500" style="margin-left:auto;margin-right:auto">';
   echo '<tr><th>Score:</th><th>Name:</th><th>Datum:</th></tr>';
   $sql = "SELECT score, name, date FROM scoreboard ORDER BY score DESC";
   foreach ($pdo->query($sql) as $row) {
