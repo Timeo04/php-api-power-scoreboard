584d7bf259d2891bd1356fe77008eac9b265f67a
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 10:48:19 +0100

    Update get.php

--- web/get.php
+++ web/get.php
@@ -1,5 +1,9 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 if(isset($_GET['num'])){
     $num = (int)$_GET['num'];
 }
