f978aea3a99e08f67663cde97d99e070be0b8934
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 01:23:16 +0100

    Update is_id_available.php

--- web/is_id_available.php
+++ web/is_id_available.php
@@ -1,13 +1,15 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$db = parse_url(getenv("DATABASE_URL"));
+
 $ok = true;
-if(isset($_GET['id']) and is_numeric($_GET['id']){
+if(isset($_GET['id']) and is_numeric($_GET['id'])){
     $id = $_GET['id'];
 }else{
   $ok = false;
   echo json_encode(["ok" => false]);
 }
+   
+$db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
@@ -16,11 +18,11 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
-/*
+
 if($ok){
    $statement = $pdo->prepare("SELECT COUNT(*) FROM scoreboard WHERE userid=".$id.");");
    $statement->execute();
    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
    echo json_encode($data);
-}*/
-echo json_encode(["ok" => true]);
+}
+//echo json_encode(["ok" => true]);
