2ac17e8e6a48c74b03dd1a1da0b42fd3bf206ada
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 11:32:41 +0100

    Update add.php

--- web/add.php
+++ web/add.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: https://preview.openprocessing.org');
 header('Access-Control-Allow-Methods: GET, POST');
 header(Access-Control-Request-Method: POST);
-//header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-Requested-With");
 $db = parse_url(getenv("DATABASE_URL"));
 $ok = true;
 
