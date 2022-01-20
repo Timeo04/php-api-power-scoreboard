80b7aa21ff9f2a4a752cf02ba75872fcffa88014
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 18:40:27 +0100

    Update admin.php

--- web/admin.php
+++ web/admin.php
@@ -23,7 +23,8 @@
     }
   
     if(isset($_POST['submitSave'])){
-     echo "<p>Saave</p>";
+     storeScore();
+     //echo "<p>Saave</p>";
     }
   
     showTable();
@@ -92,6 +93,38 @@
      $statement->execute();     
      echo "<p>zurückgesetzt!</p>";
     }
+  
+    function storeScore(){
+     $db = parse_url(getenv("DATABASE_URL"));
+     $ok = true;
+     $pdo = new PDO("pgsql:" . sprintf(
+      "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+      $db["host"],
+      $db["port"],
+      $db["user"],
+      $db["pass"],
+      ltrim($db["path"], "/")
+     ));
+     $name = $_POST['name'];
+     $score = $_POST['score'];
+     $userid = $_POST['userid'];
+     $datevar = $_POST['date'];
+     if(strlen($name)<1){
+      $ok = false;
+     }elseif(!is_numeric($score)){
+      $ok = false;
+     }elseif(!is_numeric($userid)){
+      $ok = false;
+     }
+     if(!isset($datevar)){
+       date_default_timezone_set('Europe/Zurich');
+       $datevar = date('Y-m-d H:i:s');
+     }
+     if($ok){
+      $statement = $pdo->prepare("INSERT INTO scoreboard (userid,score,name,date) VALUES (".$userid.",".$score.",'".$name."','".$datevar."');");
+      $statement->execute();
+    }
+  
    ?>
   <h2>Score eintragen</h2>
   <br>
