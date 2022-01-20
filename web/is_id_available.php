a7cd0c275d9fadda1aa556be66f46aaa3e1ef297
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 01:33:01 +0100

    Update is_id_available.php

--- web/is_id_available.php
+++ web/is_id_available.php
@@ -24,6 +24,9 @@ if($ok){
    $statement = $pdo->prepare("SELECT COUNT(*) FROM scoreboard WHERE userid=".$id.";");
    $statement->execute();
    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
-   echo json_encode($data[0]['count']);
+    if($data[0]['count']==0){
+        echo json_encode(["ok" => true]);
+    }else{
+        echo json_encode(["ok" => false]);
+    }
 }
-//echo json_encode(["ok" => true]);
