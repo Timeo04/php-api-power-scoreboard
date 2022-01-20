a00a2fdae265c0d662866a3e7caf4a67271960e5
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 13:28:59 +0100

    Update remove.php

--- web/remove.php
+++ web/remove.php
@@ -1,8 +1,9 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: https://preview.openprocessing.org');
-header('Access-Control-Allow-Methods: POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+//header('Access-Control-Allow-Origin: https://preview.openprocessing.org');
+//header('Access-Control-Allow-Methods: POST');
+//header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-PINGOTHER, Content-Type");
 
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
