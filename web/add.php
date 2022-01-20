2ddaa2520a1e0f58822d88c3d32a075fa2479570
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 11:56:10 +0100

    Update add.php

--- web/add.php
+++ web/add.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Request-Method: POST');
 //header('Access-Control-Request-Headers: Special-Request-Header'); 
 //header("Access-Control-Allow-Headers: X-Requested-With");
+header('Access-Control-Allow-Headers: x-www-form-urlencoded');
 $db = parse_url(getenv("DATABASE_URL"));
 $ok = true;
 
