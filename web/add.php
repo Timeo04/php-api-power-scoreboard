5f7d3446ac2120678d55c6b841077617c0b5b89b
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 11:52:21 +0100

    Update add.php

--- web/add.php
+++ web/add.php
@@ -4,8 +4,8 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Methods: GET, POST');
 
 header('Access-Control-Request-Method: POST');
-header('Access-Control-Request-Headers: Special-Request-Header'); 
-header("Access-Control-Allow-Headers: X-Requested-With");
+//header('Access-Control-Request-Headers: Special-Request-Header'); 
+//header("Access-Control-Allow-Headers: X-Requested-With");
 $db = parse_url(getenv("DATABASE_URL"));
 $ok = true;
 
