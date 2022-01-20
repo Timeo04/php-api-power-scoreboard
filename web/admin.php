66743224ef7016dc663e99a72975800b381846a0
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-20 14:30:42 +0100

    Create admin.php

---
+++ web/admin.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Admin - Scoreboard</title>
+  <link rel="stylesheet" href="stylesheets/style.css">
+ </head>
+ <body>
+   <h1>Admin</h1>
+   <h2>Scoreboard</h2>
+   <a href="https://openprocessing.org/sketch/1447262">zum Game</a>
+  <br>
+  <p></p>
+   <?php
+    echo "<p>Helloo World</p>";
+   ?>
+ </body>
+</html>
