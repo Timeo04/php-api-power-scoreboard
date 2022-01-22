4cedea3675630a0ba5718faf5b3ff57f0c3a618e
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-22 11:19:24 +0100

    Update get_random_userid.php

--- web/get_random_userid.php
+++ web/get_random_userid.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-global $pdo, $id;
+global $pdo;
 
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
@@ -15,17 +15,17 @@ $pdo = new PDO("pgsql:" . sprintf(
 ));
 
 $id = random_int(100000,999999);
-check($id);
+check($pdo,$id);
 
-function check($idToCheck){
-  $statement = $pdo->prepare("SELECT COUNT(*) FROM scoreboard WHERE userid=".$idToCheck.";");
+function check($pdo1,$idToCheck){
+  $statement = $pdo1->prepare("SELECT COUNT(*) FROM scoreboard WHERE userid=".$idToCheck.";");
   $statement->execute();
   $data = $statement->fetchAll(PDO::FETCH_ASSOC);
   if($data[0]['count']==0){
     echo json_encode(["id" => $idToCheck]);
   }else{
     $id = random_int(100000,999999);
-    check($id);
+    check($pdo1,$id);
     //echo json_encode(["ok" => false]);
   }
 }
