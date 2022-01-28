1103cadd7302a08809a01973c51255a2de7e494d
Timeo04 <45509216+Timeo04@users.noreply.github.com>
2022-01-28 14:52:20 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -35,8 +35,8 @@
   */
   echo '<table border="1" width="500" style="margin-left:auto;margin-right:auto">';
   echo '<tr><th>Score:</th><th>Name:</th><th>Datum:</th></tr>';
-  //$sql = "SELECT MAX(score), name FROM scoreboard GROUP BY userid, name ORDER BY score DESC";
-  $sql = "SELECT DISTINCT ON(userid) score, name, date FROM scoreboard ORDER BY score DESC";
+  $sql = "SELECT MAX(score) score, name, MAX(score) date FROM scoreboard GROUP BY userid, name ORDER BY score DESC";
+  //$sql = "SELECT DISTINCT ON(userid) score, name, date FROM scoreboard ORDER BY score DESC";
   foreach ($pdo->query($sql) as $row) {
    echo "<tr>";
    echo "<td>". $row['score'] . "</td>";
